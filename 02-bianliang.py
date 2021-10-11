f78be26d5d6cfc96f5eefa6db52730f8c8e08073
ml3082298291 <3082298291@qq.com>
2021-10-11 21:12:11 +0800

    02 认识debug

---
+++ 02-bianliang.py
@@ -0,0 +1,21 @@
+# -*- codeing = utf-8 -*-
+# @Time :2021/10/11 20:37
+# @Author :menglei
+# @File :02-bianliang.py
+# @Software: PyCharm
+
+'''
+1.定义变量
+变量名 = 值
+2.使用变量
+
+3.看变量的特点
+'''
+
+my_name = 'TOM'
+print(my_name)
+
+# 定义变量：存储数据 黑马程序员
+
+schoolName = '我是黑马程序员，我爱python'
+print(schoolName)
